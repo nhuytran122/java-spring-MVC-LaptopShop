76e66908afe61c2ffd128a25ac83e21634c6a774
nhuytran122 <nhuytran12223@gmail.com>
2024-07-10 20:26:26 +0700

    58 entity

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,5 +1,10 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +16,14 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email;
     private String password;
     private String fullName;
