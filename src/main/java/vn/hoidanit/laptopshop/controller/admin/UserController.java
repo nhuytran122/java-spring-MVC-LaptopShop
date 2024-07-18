ca25ecc1f01c80f911bd32dcf56546b655715577
nhuytran122 <nhuytran12223@gmail.com>
2024-07-18 10:19:14 +0700

    101 Validate Model & fix No mapping for GET /admin/js/scripts.js

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UploadService;
@@ -98,17 +101,24 @@ public class UserController {
 
     @PostMapping(value = "/admin/user/create")
     public String createUserPage(Model model,
-            @ModelAttribute("newUser") User hoidanit,
+            @ModelAttribute("newUser") @Valid User hoidanit,
+            BindingResult bindingResult,
             @RequestParam("hoidanitFile") MultipartFile file) {
+
+        // validate
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(">>>>>>>" + error.getObjectName() + " - " + error.getDefaultMessage());
+        }
 
         String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
         String hashPassword = this.passwordEncoder.encode(hoidanit.getPassword());
-       
+
         hoidanit.setAvatar(avatar);
         hoidanit.setPassword(hashPassword);
         hoidanit.setRole(this.userService.getRoleByName(hoidanit.getRole().getName()));
 
-        //save
+        // save
         this.userService.handleSaveServer(hoidanit);
         return "redirect:/admin/user";
     }
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -14,7 +14,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 // import lombok.ToString; : tránh lỗi StackOverflowError
+import jakarta.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -30,9 +34,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
+    @Email
     private String email;
+
+    @NotNull
+    @Min(2)
     private String password;
+
+    @NotNull
+    @Min(2)
     private String fullName;
+
     private String address;
     private String phone;
 
