258bb25619665adeb17ca2afe275862975462e44
nhuytran122 <nhuytran12223@gmail.com>
2024-07-19 22:24:01 +0700

    106 Hoàn Thiện Xem Chi Tiết Product

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -5,11 +5,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import lombok.AllArgsConstructor;
+import vn.hoidanit.laptopshop.service.ProductService;
+
+@AllArgsConstructor
 @Controller
 public class ItemController {
 
+    private final ProductService productService;
+
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
+        model.addAttribute("pr", this.productService.getProductByID(id));
         return "client/product/detail";
     }
 }
