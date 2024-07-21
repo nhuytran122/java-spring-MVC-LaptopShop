0a71f9d5300811dd58732da225dc35b170677b55
nhuytran122 <nhuytran12223@gmail.com>
2024-07-21 11:19:15 +0700

    119.2 Authorize by Role - Redirect different types of users to different pages after login

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import jakarta.servlet.DispatcherType;
 
@@ -33,6 +34,11 @@ public class SecurityConfiguration {
         }
 
         @Bean
+        public AuthenticationSuccessHandler customSuccessHandler() {
+                return new CustomSuccessHandler();
+        }
+
+        @Bean
         SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 // v6. lamda
                 http
@@ -53,6 +59,7 @@ public class SecurityConfiguration {
                                 .formLogin(formLogin -> formLogin
                                                 .loginPage("/login")
                                                 .failureUrl("/login?error")
+                                                .successHandler(customSuccessHandler())
                                                 .permitAll());
                 return http.build();
         }
