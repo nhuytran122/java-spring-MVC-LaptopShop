f2e7dfae580b699ecbd86512c5e41ea6ecd247ff
nhuytran122 <nhuytran12223@gmail.com>
2024-07-26 21:49:19 +0700

    add @Repository

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import vn.hoidanit.laptopshop.domain.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product save(Product product);
 
