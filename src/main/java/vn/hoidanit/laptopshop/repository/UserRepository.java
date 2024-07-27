6774c71c10f05541913bbba58b88e7ba37da5429
nhuytran122 <nhuytran12223@gmail.com>
2024-07-27 10:23:06 +0700

    143.2 Bài tập Pagination Users

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -2,6 +2,8 @@ package vn.hoidanit.laptopshop.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 // import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -15,7 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findOneByEmail(String email);
 
-    List<User> findAll();
+    Page<User> findAll(Pageable pageable);
 
     User findById(long id);
 
